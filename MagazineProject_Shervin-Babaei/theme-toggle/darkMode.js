3c39e79924be65e11981732d4e7db4f6b6084317
ShervinBabaei2007 <shervinb22@gmail.com>
2025-12-03 21:40:27 -0800

    added theme changer to index page only

---
+++ MagazineProject_Shervin-Babaei/theme-toggle/darkMode.js
@@ -0,0 +1,40 @@
+document.addEventListener(`DOMContentLoaded`, () => {
+  const body = document.body;
+  const toggleBtn = document.getElementById(`theme-toggle`);
+
+  // in case buttojn is not on page.
+  if (!toggleBtn) return;
+
+  // regular and dark mode
+  const LIGHT_BG = `lightskyblue`;
+  const DARK_BG = `#6495ED`; // bit more darker, better for the eyes
+
+  // save prefrence to local Storage
+  const savedTheme = localStorage.getItem(`soccerWeeklyTheme`) || `light`;
+
+  if (savedTheme === `dark`) {
+    body.style.backgroundColor = DARK_BG;
+    toggleBtn.textContent = `☀️`; // sun = bright mode (you are in dark bg)
+  } else {
+    body.style.backgroundColor = LIGHT_BG;
+    toggleBtn.textContent = `🌙`; // Moon = night mode (you are in light bg)
+  }
+
+  toggleBtn.addEventListener(`click`, () => {
+    {
+      const currentTheme = localStorage.getItem(`soccerWeeklyTheme`) || `light`;
+
+      if (currentTheme === `light`) {
+        // then switch it to dark
+        body.style.backgroundColor = DARK_BG;
+        toggleBtn.textContent = `☀️`;
+        localStorage.setItem(`soccerWeeklyTheme`, `dark`);
+      } else {
+        // switch it to light
+        body.style.background = LIGHT_BG;
+        toggleBtn.textContent = `🌙`;
+        localStorage.setItem(`soccerWeeklyTheme`, `light`);
+      }
+    }
+  });
+});
